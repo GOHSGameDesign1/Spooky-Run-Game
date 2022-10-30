8be4ce81fc5d8db973a543cc742b14f8d1d57d9f
GOHSGameDesign1 <GOHSGameDesign1@gmail.com>
2022-10-29 17:41:42 -0700

    Installed new input system

--- Spooky Running Game/Assets/Scripts/Player.cs
+++ Spooky Running Game/Assets/Scripts/Player.cs
@@ -7,7 +7,7 @@ public class Player : MonoBehaviour
     int posNum;
     float[] positions = new float[] {-3.75f, 0.7f, 5.3f};
     int score;
-
+    public float speed;
     public Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -35,7 +35,8 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        Vector2 newPos = Vector2.Lerp(rb.position, new Vector2(transform.position.x, positions[posNum]), Time.deltaTime);
+        //Vector2 newPos = Vector2.Lerp(rb.position, new Vector2(rb.position.x, positions[posNum]), Time.deltaTime * speed);
+        Vector2 newPos = new Vector2(rb.position.x, positions[posNum]);
 
         rb.MovePosition(newPos);
     }
